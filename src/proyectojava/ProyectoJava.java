19fd63a1086473d333a10caa48a6fd2ec664b3b2
alechavarria2022-code <alechavarria2022@gmail.com>
2026-03-07 14:28:09 -0600

    Funcion principal del modulo 1, todavia le falta brete

--- src/proyectojava/ProyectoJava.java
+++ src/proyectojava/ProyectoJava.java
@@ -1,11 +1,20 @@
 package proyectojava;
 
+import javax.swing.JOptionPane;
+
 public class ProyectoJava {
 
     
     public static void main(String[] args) {
-       // metodo para clase 
-       
+
+        //Esto es la parte inicial del modulo de seleccion de modelo, donde se pide la matricula y el modelo del avion, para luego imprimir la matriz del modelo seleccionado.
+        Avion a = new Avion();
+        a.setMatricula(JOptionPane.showInputDialog("Ingrese la matricula: "));
+        String modelo = JOptionPane.showInputDialog("Ingrese modelo del avión (A/B/C): ");
+        a.setModelo(modelo);
+        String[][] matriz = a.seleccionModelo();
+        a.imprimirAvion(matriz);
+    
     }
     
 }
