256e4e3136caf6ef1b4323732e4670a52f315b9f
alechavarria2022-code <alechavarria2022@gmail.com>
2026-03-07 14:33:16 -0600

    Una pequenia aclaracion

--- src/proyectojava/ProyectoJava.java
+++ src/proyectojava/ProyectoJava.java
@@ -8,6 +8,7 @@ public class ProyectoJava {
     public static void main(String[] args) {
 
         //Esto es la parte inicial del modulo de seleccion de modelo, donde se pide la matricula y el modelo del avion, para luego imprimir la matriz del modelo seleccionado.
+        // No me he sentado a estudiar swing asi que de momento se veria un poco feo, pero lo importante es que funcione, y luego ya me sentare a estudiar como hacer que se vea mejor.
         Avion a = new Avion();
         a.setMatricula(JOptionPane.showInputDialog("Ingrese la matricula: "));
         String modelo = JOptionPane.showInputDialog("Ingrese modelo del avión (A/B/C): ");
