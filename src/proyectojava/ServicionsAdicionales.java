a23e3820801c2207adc12098efde47fe8ef66cb0
MaripazFajardo <mfajardo20737@ufide.ac.cr>
2026-03-07 23:41:21 -0600

    Aporte Modulo 3, servicios adicionales

--- src/proyectojava/ServicionsAdicionales.java
+++ src/proyectojava/ServicionsAdicionales.java
@@ -10,4 +10,26 @@ package proyectojava;
  */
 public class ServicionsAdicionales {
     
+    public class Servicios {
+    private String nombre;
+    private double precio;
+    private boolean dutyFree;
+    
+    public Servicios(String nombre, double precio, boolean dutyFree){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.dutyFree = dutyFree;
+        }
+        
+        public String getNombre() {
+            return nombre;     
+        }
+        public double getPrecio() {
+            return precio;     
+        }
+      public boolean isDutyFree() {
+            return dutyFree;     
+        }
+    }
+    
 }
